d8e1550b3675664d754f3305e24168a4e0f83021
Balonbal <olavbb@hotmail.com>
2016-02-10 12:54:24 +0100

    La til variabeldefinisjon

--- Kalkulator_prosjekt/Kalkulator_prosjekt/Variabel_tolk.cs
+++ Kalkulator_prosjekt/Kalkulator_prosjekt/Variabel_tolk.cs
@@ -2,11 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Kalkulator_prosjekt
 {
     class Variabel_tolk
     {
+        private static String pattern = @"(\w[\w\d]+)=(.*)";
+        private static Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+        private static Dictionary<String, String> variables = new Dictionary<string, string>();
+
+
+        public static String getResult(String input)
+        {
+            //Define variables
+            Match match = regex.Match(input);
+            if (match.Success)
+            {
+                //Find name and value from match
+                Group name = match.Groups[1];
+                Group value = match.Groups[2];
+
+                Console.WriteLine("[DEBUG] Variabel_tolk.cs: " + name.Value + " bound to " + value.Value);
+
+                //Add to dictionary
+                variables.Add(name.Value, value.Value);
+                return "Variabel definert, gratulerer!";
+            }
+            return input;
+        }
+
     }
 }
